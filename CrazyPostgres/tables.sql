6b87bafda4b65664f1615d9f41203077ae6ecd16
Zhou Xinzi <imdreamrunner@gmail.com>
2015-10-11 20:25:17 +0800

    add create tables sql

---
+++ CrazyPostgres/tables.sql
@@ -0,0 +1,44 @@
+CREATE TABLE author (
+    aid     SERIAL      PRIMARY KEY,
+    name    TEXT        UNIQUE
+);
+
+CREATE TABLE publication (
+    pubid   SERIAL      PRIMARY KEY,
+    pubkey  TEXT        UNIQUE,
+    title   TEXT,
+    year    INTEGER
+);
+
+CREATE TABLE article (
+    pubid   INTEGER     PRIMARY KEY REFERENCES publication(pubid),
+    journal TEXT,
+    month   INTEGER,
+    volume  INTEGER,
+    number  INTEGER
+);
+
+CREATE TABLE book (
+    pubid   INTEGER     PRIMARY KEY REFERENCES publication(pubid),
+    publisher TEXT,
+    isbn    TEXT
+);
+
+CREATE TABLE incollection (
+    pubid   INTEGER     PRIMARY KEY REFERENCES publication(pubid),
+    booktitle TEXT,
+    publisher TEXT,
+    isbn    TEXT
+);
+
+CREATE TABLE inproceedings (
+    pubid   INTEGER     PRIMARY KEY REFERENCES publication(pubid),
+    booktitle TEXT,
+    editor  TEXT
+);
+
+CREATE TABLE pub_author (
+    pubid   INTEGER     REFERENCES publication(pubid),
+    aid     INTEGER     REFERENCES author(aid),
+    PRIMARY KEY (pubid, aid)
+);
