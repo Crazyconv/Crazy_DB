badabf44c63a1236a446c17e3fa0b06706e818a7
crazyconv <conanconv@163.com>
2015-10-14 13:39:08 +0800

    testdata in db postgres, tested already

--- CrazyPostgres/query_jiachun.sql
+++ CrazyPostgres/query_jiachun.sql
@@ -22,7 +22,7 @@ UNION ALL
 -- use view instead of with and build index?
 
 WITH publication_count AS (
-	SELECT aid, count(pid) AS total 
+	SELECT aid, count(pubid) AS total 
 	FROM pub_author
 	GROUP BY aid
 	ORDER BY total DESC
@@ -30,50 +30,50 @@ WITH publication_count AS (
 	)
 
 SELECT name
-FROM publication_count JOIN author USING aid;
+FROM publication_count JOIN author USING (aid);
 
 -- 2B)
--- index on pub_author.pid
+-- index on pub_author.pubid
 -- index on pub_author.aid (group by)??
 -- index on author.aid (join)
 -- use view instead of with and build index?
 
 WITH page_count AS (
 	SELECT aid, SUM(total_page) AS total 
-	FROM pub_author JOIN publication USING pubid
+	FROM pub_author JOIN publication USING (pubid)
 	GROUP BY aid
 	ORDER BY total DESC
 	LIMIT 10
 	)
 
 SELECT name
-FROM page_count JOIN author USING aid;
+FROM page_count JOIN author USING (aid);
 
 -- 3A)
 -- create index for view??
 -- index on author.aid, author.name
--- index on pub_author.aid, pub_author.pid
--- index on publication.pid, publication.year
--- index on article.pid, book.pid, incollection.pid, inproceedings.pid
+-- index on pub_author.aid, pub_author.pubid
+-- index on publication.pubid, publication.year
+-- index on article.pubid, book.pubid, incollection.pubid, inproceedings.pubid
 
-DROP VIEW special_publication IF EXIST
+DROP VIEW IF EXISTS special_publication;
 CREATE VIEW special_publication AS(
-	SELECT publication.pubid as pubid
-	FROM author JOIN pub_author ON (author.aid = pub_author.aid AND author.name = X)
-				JOIN publication ON (pub_author.pid = publication.pid AND year = 2012)
-)
+	SELECT publication.*
+	FROM author JOIN pub_author ON (author.aid = pub_author.aid AND author.name = 'conan')
+				JOIN publication ON (pub_author.pubid = publication.pubid AND year = 2012)
+);
 
-SELECT pubid, pubkey, title, year, journal, month, volumn, number 
-FROM article JOIN special_publication USING pubid;
+SELECT pubid, pubkey, title, year, journal, month, volume, number 
+FROM article JOIN special_publication USING (pubid);
 
 SELECT pubid, pubkey, title, year, publisher, isbn
-FROM book JOIN special_publication USING pubid;
+FROM book JOIN special_publication USING (pubid);
 
 SELECT pubid, pubkey, title, year, booktitle, publisher, isbn
-FROM incollection JOIN special_publication USING pubid;
+FROM incollection JOIN special_publication USING (pubid);
 
 SELECT pubid, pubkey, title, year, booktitle, editor
-FROM inproceedings JOIN special_publication USING pubid;
+FROM inproceedings JOIN special_publication USING (pubid);
 
 -- 3B)
 -- same as 3A
@@ -81,49 +81,48 @@ FROM inproceedings JOIN special_publication USING pubid;
 -- index on article.journal
 -- index on inproceedings.booktitle
 
-DROP VIEW special_publication2 IF EXIST
+DROP VIEW IF EXISTS special_publication2;
 CREATE VIEW special_publication2 AS(
-	SELECT publication.pubid as pubid
-	FROM author JOIN pub_author ON (author.aid = pub_author.aid AND author.name = X)
-				JOIN publication ON (pub_author.pid = publication.pid AND year = Y AND type = 'conf')
-)
+	SELECT publication.*
+	FROM author JOIN pub_author ON (author.aid = pub_author.aid AND author.name = 'bumei')
+				JOIN publication ON (pub_author.pubid = publication.pubid AND year = 2014 AND type = 'conf')
+);
 
-SELECT article.pubid, pubkey, title, year, journal, month, volumn, number 
-FROM article JOIN special_publication2 ON (article.journal = Z AND article.pubid = special_publication2.pubid);
+SELECT article.pubid, pubkey, title, year, journal, month, volume, number 
+FROM article JOIN special_publication2 ON (article.journal = 'conan' AND article.pubid = special_publication2.pubid);
 
-SELECT publication.pubid, pubkey, title, year, booktitle, editor
-FROM inproceedings JOIN special_publication2 ON (inproceedings.booktitle = Z AND inproceedings.pubid = special_publication2.pubid);
+SELECT inproceedings.pubid, pubkey, title, year, booktitle, editor
+FROM inproceedings JOIN special_publication2 ON (inproceedings.booktitle = 'conan' AND inproceedings.pubid = special_publication2.pubid);
 
 -- 4A)
--- index on article.journal, article.pid
--- index on pub_author.pid
--- index on inproceedings.booktitle, inproceedings.pid
+-- index on article.journal, article.pubid
+-- index on pub_author.pubid
+-- index on inproceedings.booktitle, inproceedings.pubid
 -- index on author.aid
-
-WITH PVLDB_author AS(
-	SELECT aid
-	FROM pub_author JOIN article ON (journal = 'PVLDB' AND pub_author.pid = article.pid)
-	UNION ALL
-	SELECT aid
-	FROM pub_author JOIN inproceedings ON (booktitle = 'PVLDB' AND pub_author.pid = inproceedings.pid)
-)
+DROP VIEW IF EXISTS PVLDB_count;
 CREATE VIEW PVLDB_count AS(
 	SELECT aid
-	FROM PVLDB_author
+	FROM (
+		SELECT aid
+		FROM pub_author JOIN article ON (journal = 'PVLDB' AND pub_author.pubid = article.pubid)
+		UNION ALL
+		SELECT aid
+		FROM pub_author JOIN inproceedings ON (booktitle = 'PVLDB' AND pub_author.pubid = inproceedings.pubid)
+	) AS PVLDB_author
 	GROUP BY aid
 	HAVING count(aid) >= 10
 );
 
-WITH KDD_author AS(
-	SELECT aid
-	FROM pub_author JOIN article ON (journal = 'KDD' AND pub_author.pid = article.pid)
-	UNION ALL
-	SELECT aid
-	FROM pub_author JOIN inproceedings ON (booktitle = 'KDD' AND pub_author.pid = inproceedings.pid)
-)
+DROP VIEW IF EXISTS KDD_count;
 CREATE VIEW KDD_count AS(
 	SELECT aid, count(*) AS total
-	FROM KDD_author
+	FROM (
+		SELECT aid
+		FROM pub_author JOIN article ON (journal = 'KDD' AND pub_author.pubid = article.pubid)
+		UNION ALL
+		SELECT aid
+		FROM pub_author JOIN inproceedings ON (booktitle = 'KDD' AND pub_author.pubid = inproceedings.pubid)
+	) AS KDD_author
 	GROUP BY aid
 );
 
@@ -149,74 +148,89 @@ FROM author JOIN P10K0 ON (author.aid = P10K0.aid);
 
 -- 5
 -- index on publication.year
+DROP VIEW IF EXISTS decade70;
+DROP VIEW IF EXISTS decade80;
+DROP VIEW IF EXISTS decade90;
+DROP VIEW IF EXISTS decade00;
+DROP VIEW IF EXISTS decade10;
 
-CREATE VIEW decade70 AS ( SELECT pid FROM publication WHERE year >= 1970 AND year <=1979 );
-CREATE VIEW decade80 AS ( SELECT pid FROM publication WHERE year >= 1980 AND year <=1989 );
-CREATE VIEW decade90 AS ( SELECT pid FROM publication WHERE year >= 1990 AND year <=1999 );
-CREATE VIEW decade00 AS ( SELECT pid FROM publication WHERE year >= 2000 AND year <=2009 );
-CREATE VIEW decade10 AS ( SELECT pid FROM publication WHERE year >= 2010 AND year <=2019 );
+CREATE VIEW decade70 AS ( SELECT pubid FROM publication WHERE year >= 1970 AND year <=1979 );
+CREATE VIEW decade80 AS ( SELECT pubid FROM publication WHERE year >= 1980 AND year <=1989 );
+CREATE VIEW decade90 AS ( SELECT pubid FROM publication WHERE year >= 1990 AND year <=1999 );
+CREATE VIEW decade00 AS ( SELECT pubid FROM publication WHERE year >= 2000 AND year <=2009 );
+CREATE VIEW decade10 AS ( SELECT pubid FROM publication WHERE year >= 2010 AND year <=2019 );
 
-SELECT '1970 - 1979', SUM(pid) FROM decade70;
-SELECT '1980 - 1989', SUM(pid) FROM decade80;
-SELECT '1990 - 1999', SUM(pid) FROM decade90;
-SELECT '2000 - 2009', SUM(pid) FROM decade00;
-SELECT '2010 - 2019', SUM(pid) FROM decade10;
+( SELECT '1970 - 1979' AS decade, count(pubid) FROM decade70 )
+UNION ALL
+( SELECT '1980 - 1989' AS decade, count(pubid) FROM decade80 )
+UNION ALL
+( SELECT '1990 - 1999' AS decade, count(pubid) FROM decade90 )
+UNION ALL
+( SELECT '2000 - 2009' AS decade, count(pubid) FROM decade00 )
+UNION ALL
+( SELECT '2010 - 2019' AS decade, count(pubid) FROM decade10 );
 
 -- 6
 -- index on pub_author.aid, pub_author.pubid
-
-WITH decade70_top_author AS(
-	SELECT aid, SUM(pid) AS total
-	FROM decade70 JOIN pub_author USING pubid
-	GROUP BY aid
+(
+	WITH decade70_top_author AS(
+		SELECT aid, count(pubid) AS total
+		FROM decade70 JOIN pub_author USING (pubid)
+		GROUP BY aid
+	)
+	SELECT '1970 - 1979' AS decade, name 
+	FROM decade70_top_author JOIN author ON 
+		(total = (SELECT MAX(total) FROM decade70_top_author) AND decade70_top_author.aid = author.aid)
+) UNION ALL (
+	WITH decade80_top_author AS(
+		SELECT aid, count(pubid) AS total
+		FROM decade80 JOIN pub_author USING (pubid)
+		GROUP BY aid
+	)
+	SELECT '1980 - 1989' AS decade, name 
+	FROM decade80_top_author JOIN author ON 
+		(total = (SELECT MAX(total) FROM decade80_top_author) AND decade80_top_author.aid = author.aid)
+) UNION ALL (
+	WITH decade90_top_author AS(
+		SELECT aid, count(pubid) AS total
+		FROM decade90 JOIN pub_author USING (pubid)
+		GROUP BY aid
+	)
+	SELECT '1990 - 1999' AS decade, name 
+	FROM decade90_top_author JOIN author ON 
+		(total = (SELECT MAX(total) FROM decade90_top_author) AND decade90_top_author.aid = author.aid)
+) UNION ALL (
+	WITH decade00_top_author AS(
+		SELECT aid, count(pubid) AS total
+		FROM decade00 JOIN pub_author USING (pubid)
+		GROUP BY aid
+	)
+	SELECT '2000 - 2009' AS decade, name 
+	FROM decade00_top_author JOIN author ON 
+		(total = (SELECT MAX(total) FROM decade00_top_author) AND decade00_top_author.aid = author.aid)
+) UNION ALL (
+	WITH decade10_top_author AS(
+		SELECT aid, count(pubid) AS total
+		FROM decade10 JOIN pub_author USING (pubid)
+		GROUP BY aid
+	)
+	SELECT '2010 - 2019', name 
+	FROM decade10_top_author JOIN author ON 
+		(total = (SELECT MAX(total) FROM decade10_top_author) AND decade10_top_author.aid = author.aid)
 )
-SELECT '1970 - 1979', name 
-FROM decade70_top_author JOIN author ON (total = MAX(total) AND decade70_top_author.aid = author.aid);
-
-WITH decade80_top_author AS(
-	SELECT aid, SUM(pid) AS total
-	FROM decade80 JOIN pub_author USING pubid
-	GROUP BY aid
-)
-SELECT '1980 - 1989', name 
-FROM decade80_top_author JOIN author ON (total = MAX(total) AND decade80_top_author.aid = author.aid);
-
-WITH decade90_top_author AS(
-	SELECT aid, SUM(pid) AS total
-	FROM decade90 JOIN pub_author USING pubid
-	GROUP BY aid
-)
-SELECT '1990 - 1999', name 
-FROM decade90_top_author JOIN author ON (total = MAX(total) AND decade90_top_author.aid = author.aid);
-
-WITH decade00_top_author AS(
-	SELECT aid, SUM(pid) AS total
-	FROM decade00 JOIN pub_author USING pubid
-	GROUP BY aid
-)
-SELECT '2000 - 2009', name 
-FROM decade00_top_author JOIN author ON (total = MAX(total) AND decade00_top_author.aid = author.aid);
-
-WITH decade10_top_author AS(
-	SELECT aid, SUM(pid) AS total
-	FROM decade10 JOIN pub_author USING pubid
-	GROUP BY aid
-)
-SELECT '2010 - 2019', name 
-FROM decade10_top_author JOIN author ON (total = MAX(total) AND decade10_top_author.aid = author.aid);
-
 -- 7
--- index on pub_author.aid, pub_author.pid
+-- index on pub_author.aid, pub_author.pubid
 
 WITH collaborator AS(
 	SELECT p1.aid AS aid, p2.aid AS col_aid
 	FROM pub_author p1 JOIN pub_author p2 ON (p1.pubid = p2.pubid AND NOT p1.aid = p2.aid)
 ), col_count AS(
-	SELECT aid, SUM(col_aid) AS total
+	SELECT aid, count(DISTINCT col_aid) AS total
 	FROM collaborator
 	GROUP BY aid
 )
 SELECT name
-FROM col_count JOIN author ON (total = MAX(total) AND author.aid = col_count.aid);
+FROM col_count JOIN author ON 
+	(total = (SELECT MAX(total) FROM col_count) AND author.aid = col_count.aid);
 
 
---
+++ CrazyPostgres/testdata.sql
@@ -0,0 +1,294 @@
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key1', 'title1', 2012, 'conf', 10);
+
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key2', 'title1', 2012, 'conf', 10);
+
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key3', 'title1', 2012, 'conf', 4);
+
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key4', 'title1', 2012, 'conf', 10);
+
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key5', 'title1', 2012, 'conf', 10);
+
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key6', 'title1', 2012, 'conf', 10);
+
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key7', 'title1', 2012, 'conf', 10);
+
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key8', 'title1', 2012, 'conf', 3);
+
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key9', 'title1', 2012, 'conf', 10);
+
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key10', 'title1', 2012, 'conf', 13);
+
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key11', 'title1', 2012, 'conf', 10);
+
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key12', 'title1', 2012, 'conf', 3);
+
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key13', 'title1', 2012, 'conf', 10);
+
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key14', 'title1', 2012, 'journals', 10);
+
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key15', 'title1', 2012, 'conf', 10);
+
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key16', 'title1', 2002, 'conf', 66);
+
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key17', 'title1', 1993, 'conf', 54);
+
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key18', 'title1', 1983, 'conf', 2);
+
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key20', 'title1', 1972, 'phd', 34);
+
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key21', 'title1', 1971, 'conf', 6);
+
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key22', 'title1', 2012, 'conf', 23);
+
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key23', 'title1', 2016, 'reference', 10);
+
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key24', 'title1', 2012, 'conf', 6);
+
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key25', 'title1', 2014, 'conf', 4);
+
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key26', 'title1', 2012, 'conf', 2);
+
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key27', 'title1', 2013, 'conf', 50);
+
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key28', 'title1', 2012, 'conf', 12);
+
+INSERT INTO publication (pubkey, title, year, type, total_page)
+VALUES ('key29', 'title1', 2013, 'conf', 13);
+
+
+
+INSERT INTO article (pubid, journal, month, volume, number)
+VALUES (1, 'KDD', 1, 1, 1);
+
+INSERT INTO article (pubid, journal, month, volume, number)
+VALUES (2, 'KDD', 1, 1, 1);
+
+INSERT INTO article (pubid, journal, month, volume, number)
+VALUES (3, 'KDD', 1, 1, 1);
+
+INSERT INTO article (pubid, journal, month, volume, number)
+VALUES (4, 'KDD', 1, 1, 1);
+
+INSERT INTO inproceedings (pubid, booktitle, editor)
+VALUES (5, 'KDD', 'mengxing');
+
+INSERT INTO article (pubid, journal, month, volume, number)
+VALUES (6, 'PVLDB', 1, 1, 1);
+
+INSERT INTO article (pubid, journal, month, volume, number)
+VALUES (7, 'PVLDB', 1, 1, 1);
+
+INSERT INTO article (pubid, journal, month, volume, number)
+VALUES (8, 'PVLDB', 1, 1, 1);
+
+INSERT INTO article (pubid, journal, month, volume, number)
+VALUES (9, 'PVLDB', 1, 1, 1);
+
+INSERT INTO article (pubid, journal, month, volume, number)
+VALUES (10, 'PVLDB', 1, 1, 1);
+
+INSERT INTO article (pubid, journal, month, volume, number)
+VALUES (11, 'PVLDB', 1, 1, 1);
+
+INSERT INTO article (pubid, journal, month, volume, number)
+VALUES (12, 'PVLDB', 1, 1, 1);
+
+INSERT INTO article (pubid, journal, month, volume, number)
+VALUES (13, 'PVLDB', 1, 1, 1);
+
+INSERT INTO article (pubid, journal, month, volume, number)
+VALUES (14, 'PVLDB', 1, 1, 1);
+
+INSERT INTO inproceedings (pubid, booktitle, editor)
+VALUES (15, 'PVLDB', 'jiachun');
+
+INSERT INTO inproceedings (pubid, booktitle, editor)
+VALUES (16, 'PVLDB', 'xinzi');
+
+INSERT INTO book (pubid, publisher, isbn)
+VALUES (17, 'XINZI', '11');
+
+INSERT INTO book (pubid, publisher, isbn)
+VALUES (18, 'XINZI', '11');
+
+INSERT INTO book (pubid, publisher, isbn)
+VALUES (19, 'jiachun', '11');
+
+INSERT INTO incollection (pubid, booktitle, publisher, isbn)
+VALUES (20, 'alala', 'jiachun', '11');
+
+INSERT INTO article (pubid, journal, month, volume, number)
+VALUES (21, 'huiyuan', 1, 1, 1);
+
+INSERT INTO article (pubid, journal, month, volume, number)
+VALUES (22, 'maoli', 1, 1, 1);
+
+INSERT INTO inproceedings (pubid, booktitle, editor)
+VALUES (23, 'xiaolan', 'xinzi');
+
+INSERT INTO inproceedings (pubid, booktitle, editor)
+VALUES (24, 'conan', 'xinzi');
+
+INSERT INTO inproceedings (pubid, booktitle, editor)
+VALUES (25, 'conv', 'xinzi');
+
+INSERT INTO inproceedings (pubid, booktitle, editor)
+VALUES (26, 'conan', 'xinzi');
+
+
+
+INSERT INTO pub_author (pubid, aid) VALUES (1, 1);
+INSERT INTO pub_author (pubid, aid) VALUES (2, 1);
+INSERT INTO pub_author (pubid, aid) VALUES (3, 1);
+INSERT INTO pub_author (pubid, aid) VALUES (4, 1);
+INSERT INTO pub_author (pubid, aid) VALUES (5, 1);
+INSERT INTO pub_author (pubid, aid) VALUES (6, 1);
+INSERT INTO pub_author (pubid, aid) VALUES (7, 1);
+INSERT INTO pub_author (pubid, aid) VALUES (8, 1);
+INSERT INTO pub_author (pubid, aid) VALUES (9, 1);
+INSERT INTO pub_author (pubid, aid) VALUES (10, 1);
+INSERT INTO pub_author (pubid, aid) VALUES (11, 1);
+INSERT INTO pub_author (pubid, aid) VALUES (12, 1);
+INSERT INTO pub_author (pubid, aid) VALUES (13, 1);
+INSERT INTO pub_author (pubid, aid) VALUES (14, 1);
+INSERT INTO pub_author (pubid, aid) VALUES (15, 1);
+
+INSERT INTO pub_author (pubid, aid) VALUES (1, 2);
+INSERT INTO pub_author (pubid, aid) VALUES (6, 2);
+INSERT INTO pub_author (pubid, aid) VALUES (7, 2);
+INSERT INTO pub_author (pubid, aid) VALUES (8, 2);
+INSERT INTO pub_author (pubid, aid) VALUES (9, 2);
+INSERT INTO pub_author (pubid, aid) VALUES (10, 2);
+INSERT INTO pub_author (pubid, aid) VALUES (11, 2);
+INSERT INTO pub_author (pubid, aid) VALUES (12, 2);
+INSERT INTO pub_author (pubid, aid) VALUES (13, 2);
+INSERT INTO pub_author (pubid, aid) VALUES (14, 2);
+INSERT INTO pub_author (pubid, aid) VALUES (15, 2);
+INSERT INTO pub_author (pubid, aid) VALUES (16, 2);
+
+INSERT INTO pub_author (pubid, aid) VALUES (7, 3);
+INSERT INTO pub_author (pubid, aid) VALUES (8, 3);
+INSERT INTO pub_author (pubid, aid) VALUES (9, 3);
+INSERT INTO pub_author (pubid, aid) VALUES (10, 3);
+INSERT INTO pub_author (pubid, aid) VALUES (11, 3);
+INSERT INTO pub_author (pubid, aid) VALUES (12, 3);
+INSERT INTO pub_author (pubid, aid) VALUES (13, 3);
+INSERT INTO pub_author (pubid, aid) VALUES (14, 3);
+INSERT INTO pub_author (pubid, aid) VALUES (15, 3);
+INSERT INTO pub_author (pubid, aid) VALUES (16, 3);
+
+INSERT INTO pub_author (pubid, aid) VALUES (17, 4);
+INSERT INTO pub_author (pubid, aid) VALUES (18, 5);
+INSERT INTO pub_author (pubid, aid) VALUES (19, 6);
+INSERT INTO pub_author (pubid, aid) VALUES (20, 7);
+INSERT INTO pub_author (pubid, aid) VALUES (21, 8);
+INSERT INTO pub_author (pubid, aid) VALUES (22, 9);
+INSERT INTO pub_author (pubid, aid) VALUES (24, 11);
+INSERT INTO pub_author (pubid, aid) VALUES (25, 12);
+INSERT INTO pub_author (pubid, aid) VALUES (26, 13);
+INSERT INTO pub_author (pubid, aid) VALUES (27, 14);
+INSERT INTO pub_author (pubid, aid) VALUES (28, 15);
+
+
+/* query 1: 
+            article - 15; inproceedings - 7; book - 3; incollection - 1;
+   query 2a:
+            conan
+            conv
+            xinyi
+            maolixiaowulang
+            pingci
+            yuantai
+            guangyan
+            bumei
+            zuoteng
+            boshi
+   query 2b:
+            conan
+            conv
+            xinyi
+            xiaolan
+            pingci
+            yuantai
+            guangyan
+            gaomu
+            boshi
+            mumu
+   query 3a:
+			pubid	pubkey	title	year	journal	month	volume	number
+			1		key1	title1	2012	KDD		1		1		1
+			2		key2	title1	2012	KDD		1		1		1
+			3		key3	title1	2012	KDD		1		1		1
+			4		key4	title1	2012	KDD		1		1		1
+			6		key6	title1	2012	PVLDB	1		1		1
+			7		key7	title1	2012	PVLDB	1		1		1
+			8		key8	title1	2012	PVLDB	1		1		1
+			9		key9	title1	2012	PVLDB	1		1		1
+			10		key10	title1	2012	PVLDB	1		1		1
+			11		key11	title1	2012	PVLDB	1		1		1
+			12		key12	title1	2012	PVLDB	1		1		1
+			13		key13	title1	2012	PVLDB	1		1		1
+			14		key14	title1	2012	PVLDB	1		1		1
+
+			pubid	pubkey	title	year	booktitle	editor
+			5		key5	title1	2012	KDD			mengxing
+			15		key15	title1	2012	PVLDB		jiachun
+   query 3b:
+   			pubid	pubkey	title	year	booktitle	editor
+			24		key25	title1	2014	conan		xinzi
+   query 4a:
+			name
+			conan
+   query 4b:
+			name
+			xinyi
+   query 5:
+   			decade			count
+			1970 - 1979		2
+			1980 - 1989		1
+			1990 - 1999		1
+			2000 - 2009		1
+			2010 - 2019		23
+
+   query 6:
+   			decade			name
+			1970 - 1979		pingci
+			1970 - 1979		huiyuan
+			1980 - 1989		maolixiaowulang
+			1990 - 1999		xiaolan
+			2000 - 2009		conv
+			2000 - 2009		xinyi
+			2010 - 2019		conan
+   query 7:
+			name
+			conan
+			conv
+			xinyi
+ */
